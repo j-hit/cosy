03351c6efc438c625cc623b76604cc3aad9986fd
James Bampoe <james_d_bampoe@hotmail.com>
2016-04-08 00:28:24 +0200

    feature(thermostat-view): User can change the temperature set-point

--- cosy/Shared/Thermostat.swift
+++ cosy/Shared/Thermostat.swift
@@ -8,10 +8,30 @@
 
 import Foundation
 
+enum ThermostatState {
+  case Heating
+  case Cooling
+  case Idle
+}
+
 final class Thermostat {
   var name: String
+  var currentTemperature: Int?
+  var temperatureSetPoint: Int?
+  var state: ThermostatState {
+    if (temperatureSetPoint > currentTemperature) {
+      return .Heating
+    } else if(temperatureSetPoint < currentTemperature) {
+      return .Cooling
+    } else {
+      return .Idle
+    }
+  }
   
   init(name: String) {
     self.name = name
+    // test
+    self.currentTemperature = 18
+    self.temperatureSetPoint = 23
   }
 }
--- cosy/cosy WatchKit Extension/ThermostatInterfaceController.swift
+++ cosy/cosy WatchKit Extension/ThermostatInterfaceController.swift
@@ -12,6 +12,11 @@ import Foundation
 
 class ThermostatInterfaceController: WKInterfaceController {
   
+  @IBOutlet var heatCoolLabel: WKInterfaceLabel!
+  @IBOutlet var currentTemperatureLabel: WKInterfaceLabel!
+  @IBOutlet var temperatureSetPointLabel: WKInterfaceLabel!
+  @IBOutlet var temperatureSetPointSlider: WKInterfaceSlider!
+  
   var thermostat: Thermostat? {
     didSet {
       self.setTitle(thermostat?.name)
@@ -27,9 +32,59 @@ class ThermostatInterfaceController: WKInterfaceController {
   
   override func willActivate() {
     super.willActivate()
+    reloadDataShownOnView()
   }
   
   override func didDeactivate() {
     super.didDeactivate()
+  }
+  
+  func reloadDataShownOnView() {
+    if let currentTemperature = thermostat?.currentTemperature {
+      currentTemperatureLabel.setText("now \(currentTemperature)°") // TODO: use localised string
+    } else {
+      currentTemperatureLabel.setText("--")
+    }
+    
+    if let temperatureSetPoint = thermostat?.temperatureSetPoint {
+      temperatureSetPointLabel.setText("\(temperatureSetPoint)°")
+      temperatureSetPointSlider.setValue(Float(temperatureSetPoint))
+    } else {
+      temperatureSetPointLabel.setText("--")
+      temperatureSetPointSlider.setValue(0)
+    }
+    
+    visualiseStateOfThermostat()
+  }
+  
+  func visualiseStateOfThermostat() {
+    if let state = thermostat?.state {
+      var stateDescription: String
+      switch state {
+      case .Heating:
+        stateDescription = "heat"
+        temperatureSetPointSlider.setColor(UIColor.orangeColor())
+        heatCoolLabel.setTextColor(UIColor.orangeColor())
+        temperatureSetPointLabel.setTextColor(UIColor.orangeColor())
+      case .Cooling:
+        stateDescription = "cool"
+        temperatureSetPointSlider.setColor(UIColor.blueColor())
+        heatCoolLabel.setTextColor(UIColor.blueColor())
+        temperatureSetPointLabel.setTextColor(UIColor.blueColor())
+      case .Idle:
+        stateDescription = "idle"
+        temperatureSetPointSlider.setColor(UIColor.grayColor())
+        heatCoolLabel.setTextColor(UIColor.grayColor())
+        temperatureSetPointLabel.setTextColor(UIColor.grayColor())
+      }
+      heatCoolLabel.setText(stateDescription)
+    } else {
+      heatCoolLabel.setText("--")
+    }
+  }
+  
+  @IBAction func onTemperatureSetPointChanged(value: Float) {
+    thermostat?.temperatureSetPoint = Int(value)
+    reloadDataShownOnView()
   }
 }
